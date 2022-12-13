e1bf8e99ac5ad844ba13d7c702257fab9a58e0f6
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2022-12-13 14:36:53 +0100

    några variablar och ind. filer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        int count = 0;
+
+        String word = "amogus";
+
+        String guess = "";
+
     }
 }
---
+++ src/alfredarbete.java
@@ -0,0 +1,7 @@
+public class alfredarbete {
+    public static void main(String[] args) {
+
+
+
+    }
+}
